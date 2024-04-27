cbf722e05a33ed5e28795fc447338061913339ba
Dadandan224 <114463563+Dadandan224@users.noreply.github.com>
2024-04-27 14:45:19 +0900

    クリアデータに選択中のステージ情報を追加

--- EL4S_1/Assets/ScriptableObject/StageData.cs
+++ EL4S_1/Assets/ScriptableObject/StageData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "StageData")]
 public class StageData : ScriptableObject
 {
+    public int selectStage; // 選択中のステージ
     public int n_questions = 0;
     public int n_correct = 0;
 }
