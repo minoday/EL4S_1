f8d3fc31df0be3b61cb5d5562e549ac82bde1d6d
Dadandan224 <114463563+Dadandan224@users.noreply.github.com>
2024-04-27 15:01:23 +0900

    クリア時データセット関数を実装

--- EL4S_1/Assets/ScriptableObject/StageData.cs
+++ EL4S_1/Assets/ScriptableObject/StageData.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "StageData")]
 public class StageData : ScriptableObject
 {
-    public int selectStage; // 選択中のステージ
     public int n_questions = 0;
     public int n_correct = 0;
 }
