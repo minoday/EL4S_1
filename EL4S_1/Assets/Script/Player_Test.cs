abbee0d5644ac9c3fffec67b579c2a05141b1a05
Dadandan224 <114463563+Dadandan224@users.noreply.github.com>
2024-04-27 15:39:31 +0900

    本番シーン作成

--- EL4S_1/Assets/Script/Player_Test.cs
+++ EL4S_1/Assets/Script/Player_Test.cs
@@ -7,30 +7,4 @@ public class Player_Test : MonoBehaviour
 {
     public int Score;
     public int Total;
-
-    public float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += speed * transform.forward * Time.deltaTime;
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += speed * transform.right * Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position -= speed * transform.right * Time.deltaTime;
-        }
-
-
-    }
 }
