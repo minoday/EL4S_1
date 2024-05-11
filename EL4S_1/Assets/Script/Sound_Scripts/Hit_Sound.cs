efea17e5f16926b711a232c136b12900f3f96b4b
eitooo <125841937+eitooo@users.noreply.github.com>
2024-05-11 10:51:55 +0900

    Add/Sound

--- EL4S_1/Assets/Script/Sound_Scripts/Hit_Sound.cs
+++ EL4S_1/Assets/Script/Sound_Scripts/Hit_Sound.cs
@@ -9,14 +9,22 @@ public class Hit_Sound : MonoBehaviour
     private SoundManager soundManager; //サウンドマネージャー
 
     [SerializeField]
-    private AudioClip clip1; //音源データ1
+    private Answer_checker ans;
 
-    void OnCollisionEnter(Collision other)
+    [SerializeField]
+    private AudioClip clip1; //正解音源データ1
+    [SerializeField]
+    private AudioClip clip2; //不正解音源データ1
+
+    
+
+    void OnTriggerEnter(Collider other)
     {
         //Playerにぶつかったとき
         if (other.gameObject.tag == "Player")
         {
-            soundManager.Play(clip1); //サウンドマネージャーを使用して効果音再生
+            if(ans.Correct == true)soundManager.Play(clip1); //サウンドマネージャーを使用して効果音再生
+            if(ans.Correct == false)soundManager.Play(clip2); //サウンドマネージャーを使用して効果音再生
         }
     }
 }
